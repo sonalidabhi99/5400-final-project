181b9fa06ac4a91b8081bce4bb76044f6f1ab459
kam515 <112594350+kam515@users.noreply.github.com>
2023-11-27 22:10:38 -0500

    Updates to html for keyword selection

--- Flask/app.py
+++ Flask/app.py
@@ -41,6 +41,15 @@ class Law(db.Model): # Define a Todo model for the SQLAlchemy ORM
     def __repr__(self): # Representation method to display a task's ID
         return '<Task %r>' % self.id
 
+class Output(db.Model): # Define a Todo model for the SQLAlchemy ORM
+    id = db.Column(db.Integer, primary_key=True) # Define the 'id' column as an integer and primary key
+    law_titles_included = db.Column(db.String(200), default="no laws found") # Define the 'content' column as a string with a maximum length of 200 characters, cannot be null
+    #date_created = db.Column(db.DateTime, default=datetime.utcnow) # Define 'date_created' column with a default value of the current UTC time
+    laws_summary = db.Column(db.String(200), default="no laws to summarize")#nullable=False) # instead of date_created, we will do a summary of the content
+
+    def __repr__(self): # Representation method to display a task's ID
+        return '<Task %r>' % self.id
+
 def load_data_from_csv(csv_filename):
     with open(csv_filename, 'r', newline='', encoding='utf-8') as csvfile:
         csv_reader = csv.DictReader(csvfile)
@@ -64,23 +73,49 @@ def index():
     # create a list of keywords for the multiselect
     keywords_for_mult_select = []
     for k in df['Keywords']:
-        laws_keywords = k.split(',')
-        for l in laws_keywords:
+        if type(k) == float:
+            continue
+        # try:
+        k = k.split(',')
+        k = [x.strip() for x in k]
+        # except:
+        #     continue
+        for l in k:
             # remove whitespace and non-alphanumeric characters
             l = l.strip()
-            l = ''.join(e for e in l if e.isalnum())
+            # make lowercase
+            l = l.lower()
+            #l = ''.join(e for e in l if e.isalnum())
             keywords_for_mult_select.append(l)
     keywords_for_mult_select = list(set(keywords_for_mult_select))
     keywords_for_mult_select.sort()
+    locations_for_mult_select = ['VA', 'MD', 'DC']
+    locations_for_mult_select.sort()
 
     #keywords_for_mult_select = ["keyword1", "keyword2", "keyword3"]
     if request.method == 'POST': # If the method is POST, add a new task
-        # MULTIPLE CHOICE QUESTION?        
-        selected_options = request.form.getlist('options')
-
         # filter db for regex of selected options in keywords column--return list of laws
-        laws = Law.query.with_entities(Law.law_text).filter(Law.keywords.like(f'%{selected_options}%')).all()
-        law_titles = Law.query.with_entities(Law.law_title).filter(Law.keywords.like(f'%{selected_options}%')).all()
+        from sqlalchemy import and_
+        # Get the selected options and locations from the form
+        selected_options = request.form.getlist('options')
+        location_selected = request.form.getlist('location')
+
+        # Filter the database based on selected options and locations
+        # Assuming Law.location is the column for location in the Law model
+        laws = Law.query.with_entities(Law.law_text).filter(
+            and_(
+                Law.keywords.like(f'%{selected_options}%'),
+                Law.location.in_(location_selected)
+            )
+        ).all()
+
+        law_titles = Law.query.with_entities(Law.law_title).filter(
+            and_(
+                Law.keywords.like(f'%{selected_options}%'),
+                Law.location.in_(location_selected)
+            )
+        ).all()
+
 
         task_content = request.form['content'] # Retrieve the task content from the form data
         # retrieve the summary of the task content
@@ -88,9 +123,12 @@ def index():
         content_summary_from_model = summarizer(task_content, min_length=10, max_length=25,
            clean_up_tokenization_spaces=True)[0]['summary_text']
         new_task = Todo(content=task_content, content_summary=content_summary_from_model) # Create a new Todo object with the task content
-
+        new_law_query = Output(law_titles_included=law_titles, laws_summary=laws) 
         try:
             db.session.add(new_task) # Add the new task to the database session and commit
+            db.session.commit()
+            # add the new law query to the database session and commit
+            db.session.add(new_law_query) # Add the new task to the database session and commit
             db.session.commit()
             return redirect('/') # Redirect to the home page after adding the task
         except:
@@ -99,7 +137,8 @@ def index():
     else: # If the method is GET, display current tasks
         #tasks = Todo.query.order_by(Todo.date_created).all() # Query the database for all tasks, ordered by creation date
         tasks = Todo.query.all()
-        return render_template('index.html', tasks=tasks, keywords=keywords_for_mult_select) # Render the index template with the list of tasks
+        outputs = Output.query.all()
+        return render_template('index.html', tasks=tasks, keywords=keywords_for_mult_select, states=locations_for_mult_select, outputs=outputs) # Render the index template with the list of tasks
 
 
 @app.route('/delete/<int:id>') # Define the delete route for a task, identified by its id
@@ -118,10 +157,32 @@ def update(id):
     task = Todo.query.get_or_404(id) # Retrieve the task by id or return 404 if not found
 
     if request.method == 'POST': # If the method is POST, update the task
+        from sqlalchemy import and_
+
+        # Get the selected options and locations from the form
+        selected_options = request.form.getlist('options')
+        location_selected = request.form.getlist('location')
+
+        # Filter the database based on selected options and locations
+        # Assuming Law.location is the column for location in the Law model
+        laws = Law.query.with_entities(Law.law_text).filter(
+            and_(
+                Law.keywords.like(f'%{selected_options}%'),
+                Law.location.in_(location_selected)
+            )
+        ).all()
+
+        law_titles = Law.query.with_entities(Law.law_title).filter(
+            and_(
+                Law.keywords.like(f'%{selected_options}%'),
+                Law.location.in_(location_selected)
+            )
+        ).all()
+
         task.content = request.form['content'] # Update the task content with the form data
         # update the summary of the task content
         summarizer = pipeline("summarization")
-        content_summary_from_model = summarizer(task.content, min_length=10, max_length=25,
+        content_summary_from_model = summarizer(task.content, min_length=100, max_length=300,
            clean_up_tokenization_spaces=True)[0]['summary_text']
         task.content_summary = content_summary_from_model
         try:
@@ -131,7 +192,7 @@ def update(id):
             return 'There was an issue updating your task' # Return an error message if there's an issue updating the task
 
     else:
-        return render_template('update.html', task=task, content_summary=content_summary_from_model) # If the method is GET, render the update template with the task data
+        return render_template('update.html', task=task, content_summary=content_summary_from_model, laws=laws, law_titles=law_titles) # If the method is GET, render the update template with the task data
 
 
 if __name__ == "__main__": # Conditional to ensure the script runs only if it is the main program
